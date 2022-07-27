f79921eda1c31b3e3073ed339dc753f874757965
Jorge Luis <jorgeluis21.duran@gmail.com>
2022-07-27 16:51:03 -0400

    Red - Once again our tests are not passing propertly, and we've got to write production code to make it pass again

--- __test__/fizzbuzz.spec.js
+++ __test__/fizzbuzz.spec.js
@@ -3,15 +3,16 @@ const { fizzbuzz } = require('../src/fizzbuzz')
 
 describe("fizzbuzz", () => {
    
-   describe("Input is multiple of 3", () => {
-        let spyOnConsole; 
-	beforeEach(() => {
+     let spyOnConsole;
+        beforeEach(() => {
            spyOnConsole = jest.spyOn(console, 'log');
-	})
+        })
 
-	afterEach(() => {
-	   jest.clearAllMocks();
-	})
+        afterEach(() => {
+           jest.clearAllMocks();
+        })
+
+   describe("Input is multiple of 3", () => {
 
    	it("should print 'Fizz' when input is multiple of 3", () => {
 		fizzbuzz(3);
@@ -23,5 +24,19 @@ describe("fizzbuzz", () => {
         	expect(spyOnConsole).not.toHaveBeenCalledWith('Fizz')
    	})
    
+   })
+
+     describe("Input is multiple of 5", () => {
+
+        it("should print 'Buzz' when input is multiple of 5", () => {
+                fizzbuzz(5);
+                expect(spyOnConsole).toHaveBeenCalledWith('Buzz')
+        })
+
+        it("should not print 'Buzz' when input is not multiple of 5", () => {
+                fizzbuzz(2);
+                expect(spyOnConsole).not.toHaveBeenCalledWith('Buzz')
+        })
+
    })
 })
